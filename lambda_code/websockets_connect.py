f68bf079eafb08f7cf17fb82c1db3f31abfa576d
Joshua Glover <glover.jjs@gmail.com>
2024-11-08 10:45:54 +0000

    Creating VPC lambdas, elasticache, half done lambda code

--- lambda_code/websockets_connect.py
+++ lambda_code/websockets_connect.py
@@ -1,10 +1,21 @@
 import boto3
 from os import environ
-
+import ssl
+from pymemcache.client.base import Client
 
 def handler(event, context):
     print(event)
     print(context)
+
+
+
+    context = ssl.create_default_context()
+    cluster_endpoint = "websockets-cache-ax0tfv.serverless.euw1.cache.amazonaws.com"
+    target_port = 11211
+    memcached_client = Client((f"{cluster_endpoint}", target_port), tls_context=context)
+    memcached_client.set("key", "value", expire=500, noreply=False)
+    assert memcached_client.get("key").decode() == "value"
+
     dynamodb = boto3.resource('dynamodb')
 
     table = dynamodb.Table(environ.get("DYNAMODB_TABLE_NAME", ""))
