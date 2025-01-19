67c5460d70510c8f5336b498e2a5edde0ad38cac
Joshua Glover <glover.jjs@gmail.com>
2025-01-19 14:41:30 +0000

    Changing log levels object

--- lambda_code/lambda_logging.py
+++ lambda_code/lambda_logging.py
@@ -3,12 +3,7 @@ import logging
 
 def log_config(filename: str, log_level="INFO"):
     log_level = str.upper(log_level)
-    log_levels = {
-        'DEBUG': logging.DEBUG,
-        'INFO': logging.INFO,
-        'WARN': logging.WARN,
-        'ERROR': logging.ERROR,
-    }
+    log_levels = ['DEBUG', 'INFO', 'WARN', 'ERROR']
 
     if log_level not in log_levels:
         raise (ValueError(f"Invalid log level: {log_level}"))
